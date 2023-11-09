a804c9cd4a2b0566a480ae8078618a8b3cd5b4f5
MohamedGoush <0x1110111@gmail.com>
2023-11-09 23:37:08 +0530

    Initial Commit

---
+++ Each Messagenger/Each_MessagengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Each_MessagengerApp.swift
+//  Each Messagenger
+//
+//  Created by python on 09/11/23.
+//
+
+import SwiftUI
+
+@main
+struct Each_MessagengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
